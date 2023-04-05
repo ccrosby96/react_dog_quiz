96b2f64de22114d6205f5bb715e31a46f623a707
Calvin Crosby <calvincrosby@Calvins-Air.mshome.net>
2023-04-04 22:09:31 -0400

    added bootstrap, reformatted front page

--- src/quiz.js
+++ src/quiz.js
@@ -3,24 +3,50 @@ import './App.css';
 
 function Quiz() {
     return (
-        <div className="quiz">
+        <div className="row">
+            <div className= "col-2">
 
-            <h1>My dogs!</h1>
+            </div>
+            <div className= "col-8">
+                <h1 className = "d-flex justify-content-center">My dogs!</h1>
 
-            <h3>Meet my 5 dogs!</h3>
-            <img src="/Images/Grouppic.jpg" width="500" height="350" />
-            <p>Click on their profiles to learn more:</p>
-            <a href="/pset2/Pards.html">Pards</a>
-            <a href="/pset2/Baby.html">Baby</a>
-            <a href="/pset2/Zeus.html">Zeus</a>
-            <a href="/pset2/joey_juliette.html">Joey and Juliette</a>
+                <h3 className = "d-flex justify-content-center">Meet my 5 dogs!</h3>
+                <div className="text-center">
+                    <img src="/Images/Grouppic.jpg"  className= "img-fluid" />
+                </div>
 
-            <p>
-                <strong>Find out which dog you are here:</strong>
-                <a href="/pset2/Quiz4.html">Quiz</a>
-            </p>
+                <p>Click on their profiles to learn more:</p>
+                <ul className="nav nav-pills nav-fill">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/pset2/Pards.html">Pards</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="pset2/Baby.html">Baby</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/pset2/Zeus.html">Zeus</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/pset2/joey_juliette.html">Joey and Juliette</a>
+                    </li>
+                </ul>
 
-        </div>
+
+                <p>
+                    <strong>Find out which dog you are here:</strong>
+                    <a className="btn btn-primary ms-2" href="/pset2/Quiz4.html" role="button">Quiz</a>
+                </p>
+
+
+            </div>
+            <div className= "col-2" >
+
+            </div>
+
+
+            </div>
+
+
     );
 }
 
